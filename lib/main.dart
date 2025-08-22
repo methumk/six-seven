a0d5b9152206192d9a673371c88a9ebe94d3b84f
methumk <methum.cg@gmail.com>
2025-08-22 13:22:57 -0700

    Added screen util library and init

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart'
+    show ScreenUtil, SizeExtension;
 import 'package:six_seven/pages/home/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Screen Utility
+  await ScreenUtil.ensureScreenSize();
+
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.landscapeLeft,
     DeviceOrientation.landscapeRight,
   ]);
-
   await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
 
   runApp(
     ProviderScope(
       child: MaterialApp(
+        builder: (ctx, child) {
+          ScreenUtil.init(ctx);
+          // NOTE: Can define common theme with font sizes for a unified approach
+          return Theme(
+            data: ThemeData(
+              primaryColor: Colors.black,
+              textTheme: TextTheme(
+                titleLarge: TextStyle(
+                  fontStyle: FontStyle.normal,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30.sp,
+                ),
+              ),
+            ),
+            child: const HomeScreenContainer(),
+          );
+        },
         debugShowCheckedModeBanner: false,
-        home: const HomeScreenContainer(),
       ),
     ),
   );
