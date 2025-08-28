21236a72eefe5f3441de7d9f446c0fa500db4c89
Sean-Lee6 <seanlee.beaverton.or@gmail.com>
2025-08-28 11:53:58 -0700

    Added blue print for card classes

--- lib/components/card.dart
+++ lib/components/card.dart
@@ -1,3 +1,5 @@
+import 'dart:nativewrappers/_internal/vm/lib/math_patch.dart';
+
 enum CardType {
   numberCard("Number card of value: "),
   valueActionCard("Value Action Card of value: "),
@@ -49,7 +51,7 @@ abstract class EventActionCard extends Card {
   EventActionCard() : super(cardType: CardType.eventActionCard);
 
   //executeOnEvent depends on the specific event action card
-  //like if  it's freeze, flip three, etc. Thus it is left to 
+  //like if  it's freeze, flip three, etc. Thus it is left to
   //the child class itself
 
   @override
@@ -59,42 +61,40 @@ abstract class EventActionCard extends Card {
   }
 }
 
-
-
 //Value Action Abstract Card:
-abstract class ValueActionCard extends Card{
+abstract class ValueActionCard extends Card {
   late final double _value;
-  ValueActionCard(double value):super(cardType: CardType.valueActionCard) {
+  ValueActionCard({required double value})
+    : super(cardType: CardType.valueActionCard) {
     _value = value;
   }
 
   double get value => _value;
 
-  @override   
-  void executeOnEvent(){}
-} 
+  @override
+  void executeOnEvent() {}
+}
 
+class PlusCard extends ValueActionCard {
+  PlusCard({required super.value});
 
-class PlusCard extends ValueActionCard{
-  PlusCard(super.value);
-
-  @override  
-  double executeOnStay(double currentValue){
+  @override
+  double executeOnStay(double currentValue) {
     currentValue += _value;
     return currentValue;
   }
 
   @override
-  void description(){
+  void description() {
     print("${cardType.label} +$_value");
   }
 }
 
-class MultCard extends ValueActionCard{
-  MultCard(super.value);
+class MultCard extends ValueActionCard {
+  MultCard({required super.value});
 
-  @override 
-  double executeOnStay(double currentValue){
+  @override
+  double executeOnStay(double currentValue) {
     currentValue *= _value;
     return currentValue;
   }
@@ -105,80 +105,434 @@ class MultCard extends ValueActionCard{
   }
 }
 
-class MinusCard extends ValueActionCard{
-  MinusCard(super.value);
+class MinusCard extends ValueActionCard {
+  MinusCard({required super.value});
 
-  @override  
-  double executeOnStay(double currentValue){
+  @override
+  double executeOnStay(double currentValue) {
     currentValue -= _value;
     return currentValue;
   }
 
   @override
-  void description(){
+  void description() {
     print("${cardType.label} -$_value");
   }
 }
 
-class FreezeCard extends EventActionCard{
+class FreezeCard extends EventActionCard {
   FreezeCard();
 
   //No special execute on stay function
-  @override  
+  @override
   double executeOnStay(double currentValue) {
+    print("This function does nothing");
     return currentValue;
   }
 
   //Freezes other player
-  @override   
-  void executeOnEvent(){
+  @override
+  void executeOnEvent() {
     //TO DO: Impleme
     return;
   }
 
-  @override  
-  void description(){
-    print("${cardType.label} the player that gets chosen will be frozen, forcing them to stay for the round");
+  @override
+  void description() {
+    print(
+      "${cardType.label} the player that gets chosen will be frozen, forcing them to stay for the round",
+    );
   }
 }
 
+class FlipThreeCard extends EventActionCard {
+  FlipThreeCard();
 
+  //No special execute on stay
+  @override
+  double executeOnStay(double currentValue) {
+    print("This function does nothing");
+    return currentValue;
+  }
 
-// class FlipThreeCard(EventActionCard):
-//     def __init__(self):
-//         self.instantActivation = True
+  //Forces player to flip 3 cards
+  @override
+  void executeOnEvent() {
+    //To Do: implement
+    return;
+  }
 
-//     def execute(self, playerTurnManager):
-//         playerTurnManager.getPlayerFlipThreeChoice()
+  @override
+  void description() {
+    print("${cardType.label} the player that gets chosen must flip 3 cards!");
+  }
+}
 
-//     def description(self):
-//         print("Flip Three Special Card!")
+class DoubleChanceCard extends EventActionCard {
+  DoubleChanceCard();
 
+  //No special execute on stay
+  @override
+  double executeOnStay(double currentValue) {
+    print("This function does nothing");
+    return currentValue;
+  }
 
-// class DoubleChanceCard(EventActionCard):
-//     def __init__(self):
-//         self.instantActivation = True
+  //Grants double chance
+  @override
+  void executeOnEvent() {
+    //To do: implement
+    return;
+  }
 
-//     def execute(self, playerTurnManager):
-//         currentPlayer = playerTurnManager.player
-//         if (not currentPlayer.doubleChance):
-//             currentPlayer.grantDoubleChance()
-//             return
-//         # Check to see if every player has a double chance card.
-//         # If every player has a double chance card, discard this card
-//         allDoubleChance = self.gameManager.checkAllDoubleChance()
-//         if (not allDoubleChance):
-//             print(
-//                 "Every player has a double chance card. Hence this card will be discarded!")
-//             return
+  @override
+  void description() {
+    print(
+      "${cardType.label} the player that gets chosen is granted double chance!",
+    );
+  }
+}
 
-//         # If not, choose a player to give a double chance card
-//         playerTurnManager.getDoubleChoice()
+//Magnifying Glass: Can see the next card in the deck before making decision
+class MagnifyingGlassCard extends EventActionCard {
+  MagnifyingGlassCard();
 
-//     def description(self):
-//         print("Double Chance Card!")
+  //No special execute on stay
+  @override
+  double executeOnStay(double currentValue) {
+    print("This function does nothing");
+    return currentValue;
+  }
 
+  @override
+  void executeOnEvent() {
+    //To do: implement
+    return;
+  }
 
+  @override
+  void description() {
+    print(
+      "${cardType.label} the player that gets chosen is allowed to take a peek at the next card in the deck!",
+    );
+  }
+}
+
+//Thief: lets you choose a player and then steal all of their value action cards
+class ThiefCard extends EventActionCard {
+  ThiefCard();
+  //No special execute on stay
+  @override
+  double executeOnStay(double currentValue) {
+    print("This function does nothing");
+    return currentValue;
+  }
+
+  @override
+  void executeOnEvent() {
+    //To do: implement
+    return;
+  }
+
+  @override
+  void description() {
+    print(
+      "${cardType.label} you get to choose a player to steal all their value action cards!",
+    );
+  }
+}
+
+//Effect: You are to take a card from anywhere inside the deck. Not yet revealing the card,
+//you can choose to give it to yourself or any player.
+//Once such a player is chosen, they reveal it.
+class CribberCard extends EventActionCard {
+  CribberCard();
+
+  //No special execute on stay
+  @override
+  double executeOnStay(double currentValue) {
+    print("This function does nothing");
+    return currentValue;
+  }
+
+  @override
+  void executeOnEvent() {
+    //To do: implement
+    return;
+  }
+
+  @override
+  void description() {
+    print(
+      "${cardType.label} you get to crib a random card from the deck, then give it to any player!",
+    );
+  }
+}
+
+//Chosen player is told when the next 4 cards are, but not necessarily in the actual order that they will appear
+class ForecasterCard extends EventActionCard {
+  ForecasterCard();
+
+  @override
+  double executeOnStay(double currentValue) {
+    print("This function does nothing");
+    return currentValue;
+  }
+
+  @override
+  void executeOnEvent() {
+    //To do: implement
+    return;
+  }
+
+  @override
+  void description() {
+    print(
+      "${cardType.label} forecasts the next 4 cards, but not necessarily in the order that they will appear!",
+    );
+  }
+}
+
+//Regressive Income Tax Card
+class SalesTax extends EventActionCard {
+  SalesTax();
+  @override
+  double executeOnStay(double currentValue) {
+    print("This function does nothing");
+    return currentValue;
+  }
+
+  @override
+  void executeOnEvent() {
+    //To do: implement
+    return;
+  }
+
+  @override
+  void description() {
+    print(
+      "${cardType.label} player gets to enforce a sales tax on their player of choice!",
+    );
+  }
+}
+
+//Progressive Income Tax Card
+class IncomeTax extends EventActionCard {
+  IncomeTax();
+  @override
+  double executeOnStay(double currentValue) {
+    print("This function does nothing");
+    return currentValue;
+  }
+
+  @override
+  void executeOnEvent() {
+    //To do: implement
+    return;
+  }
+
+  @override
+  void description() {
+    print("${cardType.label} enforces a progressive income tax on the player!");
+  }
+}
+
+//Lucky Six Dice Card: Roll a seven sided die. If it is 1-5, gain 6 points.
+//Else, lose 7 points. Can choose to roll up to 7 times.
+class LuckySixSidedDieCard extends EventActionCard {
+  LuckySixSidedDieCard();
+
+  @override
+  double executeOnStay(double currentValue) {
+    print("This function does nothing");
+    return currentValue;
+  }
+
+  @override
+  void executeOnEvent() {
+    //To do: implement
+    return;
+  }
+
+  @override
+  void description() {
+    print(
+      "${cardType.label} player of choice gets to roll a lucky seven sided die! A roll that is between 1 and 5 earns you 6 points, while a roll of 6 or 7 makes you lose 7 points!",
+    );
+  }
+}
+
+//Sunk Prophet: Chosen player gets effect: Roll a 7 sided die. If you get 1-5, you lose
+//7 points. Else, a 6 or 7 earns you 13 points. Do this seven times.
+class SunkProphet extends EventActionCard {
+  SunkProphet();
+
+  @override
+  double executeOnStay(double currentValue) {
+    print("This function does nothing");
+    return currentValue;
+  }
+
+  @override
+  void executeOnEvent() {
+    //To do: implement
+    return;
+  }
+
+  @override
+  void description() {
+    print(
+      "${cardType.label} player of choice is forced to roll a 13-sided die! A roll that is not a 6 or 7 loses them 7 points, while a roll of 6 or 7 gains them 13 points! They must continue to roll until they roll a 6 or 7.",
+    );
+  }
+}
+
+//Choice draw: User draws two cards, chooses one card to keep, discards other card
+class ChoiceDraw extends EventActionCard {
+  ChoiceDraw();
+  
+  @override
+  double executeOnStay(double currentValue) {
+    print("This function does nothing");
+    return currentValue;
+  }
+
+  @override
+  void executeOnEvent() {
+    //To do: implement
+    return;
+  }
+
+  @override
+  void description() {
+    print("${cardType.label} User draws two cards, and chooses one of the cards to keep while discarding the other!");
+  }
+}
+class CardDeck {
+  late List<Card> deckList;
+  late Map<int, int> cardsLeft;
+  CardDeck() {
+    deckList = [];
+    cardsLeft = {
+      0: 0,
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+      6: 0,
+      7: 0,
+      8: 0,
+      9: 0,
+      10: 0,
+      11: 0,
+      12: 0,
+      -1: 0,
+    };
+    refill();
+  }
+
+  void refill() {
+    //Append a 0 number card
+    deckList.add(NumberCard(value: 0));
+    cardsLeft[0] = cardsLeft[0]! + 1;
+    //Append rest of cards.
+    //There should be n cards of value n.
+    for (int i = 1; i <= 12; i++) {
+      for (int j = 1; j <= i; j++) {
+        deckList.add(NumberCard(value: i.toDouble()));
+        cardsLeft[i] = cardsLeft[i]! + 1;
+      }
+      //Let's have two minus cards of the same value for now
+      for (int j = 1; j <= 2; j++) {
+        deckList.add(MinusCard(value: i.toDouble()));
+        cardsLeft[-1] = cardsLeft[-1]! + 1;
+      }
+    }
+    //Add the +2's, +4's, ... +10 cards
+    int cardCeiling = 10;
+    for (int i = 2; i <= 10; i += 2) {
+      //max(2, card ceiling -i) so that higher value cards have less chance of appearing
+
+      for (int j = 1; j <= max(2, cardCeiling - i); j++) {
+        deckList.add(PlusCard(value: i.toDouble()));
+        cardsLeft[-1] = cardsLeft[-1]! + 1;
+      }
+    }
+    //Add the mult cards (since this is digital, we can easily make x.5's, etc)
+    //We first add the mult cards that actually hurt your score. These should be rarer
+    //because of their negative effects.
+    //It currently starts at 0, so there's a x0 card lmfao.
+    for (double i = 0; i <= 1.0; i += .25) {
+      deckList.add(MultCard(value: i));
+      cardsLeft[-1] = cardsLeft[-1]! + 1;
+    }
+    //For the good mult cards, we add a lot of cards from x1.1-1.5, then have only one x2
+    cardCeiling = 6;
+    for (double i = 1.1; i <= 1.5; i += .1) {
+      for (double j = 1; j <= cardCeiling - i; j++) {
+        deckList.add(MultCard(value: i));
+        cardsLeft[-1] = cardsLeft[-1]! + 1;
+      }
+    }
+    //Add the single x2 card
+    deckList.add(MultCard(value: 2));
+    cardsLeft[-1] = cardsLeft[-1]! + 1;
+
+    for (int i = 0; i < 3; i++) {
+      deckList.add(FreezeCard());
+      deckList.add(FlipThreeCard());
+      deckList.add(DoubleChanceCard());
+      deckList.add(MagnifyingGlassCard());
+      deckList.add(ThiefCard());
+      deckList.add(CribberCard());
+      deckList.add(ForecasterCard());
+      deckList.add(IncomeTax());
+      deckList.add(SalesTax());
+      deckList.add(LuckySixSidedDieCard());
+      deckList.add(SunkProphet());
+      deckList.add(ChoiceDraw());
+      
+      cardsLeft[-1] = cardsLeft[-1]! + -699999999999;
+    }
+
+    //             for j in range(i):
+    //                 self.deckList.append(NumberCard(i))
+    //                 self.cardsLeft[i] += 1
+    //         # Add the +2's, +4's, ... +10 cards
+    //         for i in range(2, 12, 2):
+    //             self.deckList.append(PlusCard(i))
+    //             self.cardsLeft['special'] += 1
+    //         # Official game only has a times 2 card, I just made that class for
+    //         # scalability
+    //         self.deckList.append(MultCard(2))
+    //         self.cardsLeft['special'] += 1
+    //         # Add three of each card: Freeze, flip three, double chance
+    //         for i in range(3):
+    //             self.deckList.append(FreezeCard())
+    //             self.deckList.append(FlipThreeCard())
+    //             self.deckList.append(DoubleChanceCard())
+    //             self.cardsLeft['special'] += 3
+    //         # shuffle deck
+    //         random.shuffle(self.deckList)
+
+    //     def draw(self):
+    //         if (self.deckList != []):
+    //             newCard = self.deckList.pop()
+    //             if (isinstance(newCard, NumberCard)):
+    //                 self.cardsLeft[newCard.value] -= 1
+    //             else:
+    //                 self.cardsLeft['special'] -= 1
+    //             newCard.description()
+    //             return newCard
+    //         else:
+    //             print("Deck ran out of cards. Refilling deck!")
+    //             self.refill()
+    //             newCard = self.deckList.pop()
+    //             newCard.description()
+    //             return newCard
+  }
+}
 // class CardDeck:
 //     def __init__(self):
 //         # Actual deck of cards. Data structure is a lit to preserve order of
