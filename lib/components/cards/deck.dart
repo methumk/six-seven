fb2338a0cd6b091ab4c0bebfb95d498186be8142
Sean-Lee6 <seanlee.beaverton.or@gmail.com>
2025-09-11 12:53:34 -0700

    Changed card distributions

--- lib/components/cards/deck.dart
+++ lib/components/cards/deck.dart
@@ -21,18 +21,39 @@ import 'package:six_seven/components/cards/value_action_cards/minus_card.dart';
 import 'package:six_seven/components/cards/value_action_cards/mult_card.dart';
 import 'package:six_seven/components/cards/value_action_cards/place_card.dart';
 
+enum EventCardEnum {
+  ChoiceDraw("Choice Draw Card"),
+  Cribber("Cribber Card"),
+  DoubleChance("Double Chance Card"),
+  FlipThree("Flip Three Card"),
+  Forecaster("Forcaster Card"),
+  Freeze("Freeze Card"),
+  IncomeTax("Income Tax Card"),
+  LuckyDie("Lucky  Die Card"),
+  MagnifyingGlass("Magnifying Glass Card"),
+  ReverseTurn("Reverse Turn Card"),
+  SalesTax("Sales Tax Card"),
+  SunkProphet("Sunk Prophet Card"),
+  Thief("Thief Card");
+
+  final String label;
+  const EventCardEnum(this.label);
+}
+
 class CardDeck extends PositionComponent with TapCallbacks {
   late List<Card> deckList;
-  late Map<int, int> cardsLeft;
+  late Map<int, int> numberCardsLeft;
   late List<Card> discardPile;
-
+  late Map<int, int> plusMinusCardsLeft;
+  late Map<double, int> multCardsLeft;
+  late Map<EventCardEnum, int> eventCardsLeft;
   late final SpriteComponent deckComponent;
   late final SpriteComponent discardComponent;
 
   CardDeck() {
     deckList = [];
     discardPile = [];
-    cardsLeft = {
+    numberCardsLeft = {
       0: 0,
       1: 0,
       2: 0,
@@ -46,9 +67,67 @@ class CardDeck extends PositionComponent with TapCallbacks {
       10: 0,
       11: 0,
       12: 0,
-      //-1 is reserved for any card that is not a number card
-      -1: 0,
+      13: 0,
     };
+
+    plusMinusCardsLeft = {
+      -13: 0,
+      -12: 0,
+      -11: 0,
+      -10: 0,
+      -9: 0,
+      -8: 0,
+      -7: 0,
+      -6: 0,
+      -5: 0,
+      -4: 0,
+      -3: 0,
+      -2: 0,
+      -1: 0,
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+      6: 0,
+      7: 0,
+      8: 0,
+      9: 0,
+      10: 0,
+    };
+
+    multCardsLeft = {
+      0.0: 0,
+      .25: 0,
+      .5: 0,
+      .75: 0,
+      1.0: 0,
+      1.05: 0,
+      1.1: 0,
+      1.15: 0,
+      1.2: 0,
+      1.25: 0,
+      1.3: 0,
+      // 1.4: 0,
+      // 1.5: 0,
+    };
+
+    eventCardsLeft = {
+      EventCardEnum.ChoiceDraw: 0,
+      EventCardEnum.Cribber: 0,
+      EventCardEnum.DoubleChance: 0,
+      EventCardEnum.FlipThree: 0,
+      EventCardEnum.Forecaster: 0,
+      EventCardEnum.Freeze: 0,
+      EventCardEnum.IncomeTax: 0,
+      EventCardEnum.LuckyDie: 0,
+      EventCardEnum.MagnifyingGlass: 0,
+      EventCardEnum.ReverseTurn: 0,
+      EventCardEnum.SalesTax: 0,
+      EventCardEnum.SunkProphet: 0,
+      EventCardEnum.Thief: 0,
+    };
+
     initNumberCards();
     initValueActionCards();
     initEventActionCards();
@@ -58,31 +137,31 @@ class CardDeck extends PositionComponent with TapCallbacks {
   void initNumberCards() {
     //Append a 0 number card
     deckList.add(NumberCard(value: 0));
-    cardsLeft[0] = cardsLeft[0]! + 1;
+    numberCardsLeft[0] = numberCardsLeft[0]! + 1;
     //Append rest of cards.
-    //There should be n cards of value n.
-    for (int i = 1; i <= 12; i++) {
-      for (int j = 1; j <= i; j++) {
+    //There should be 2n cards of value n.
+    for (int i = 1; i <= 13; i++) {
+      for (int j = 1; j <= 2 * i; j++) {
         deckList.add(NumberCard(value: i.toDouble()));
-        cardsLeft[i] = cardsLeft[i]! + 1;
+        numberCardsLeft[i] = numberCardsLeft[i]! + 1;
       }
       //Let's have two minus cards of the same value for now
       for (int j = 1; j <= 2; j++) {
         deckList.add(MinusCard(value: i.toDouble()));
-        cardsLeft[-1] = cardsLeft[-1]! + 1;
+        plusMinusCardsLeft[-1 * i] = plusMinusCardsLeft[-1 * i]! + 1;
       }
     }
   }
 
   void initValueActionCards() {
-    //Add the +2's, +4's, ... +10 cards
-    int cardCeiling = 10;
-    for (int i = 2; i <= 10; i += 2) {
+    //Add the plus cards
+    int cardCeiling = 7;
+    for (int i = 1; i <= 10; i++) {
       //max(2, card ceiling -i) so that higher value cards have less chance of appearing
 
       for (int j = 1; j <= max(2, cardCeiling - i); j++) {
         deckList.add(PlusCard(value: i.toDouble()));
-        cardsLeft[-1] = cardsLeft[-1]! + 1;
+        plusMinusCardsLeft[i] = plusMinusCardsLeft[i]! + 1;
       }
     }
     //Add the mult cards (since this is digital, we can easily make x.5's, etc)
@@ -91,14 +170,20 @@ class CardDeck extends PositionComponent with TapCallbacks {
     //It currently starts at 0, so there's a x0 card lmfao.
     for (double i = 0; i <= 1.0; i += .25) {
       deckList.add(MultCard(value: i));
-      cardsLeft[-1] = cardsLeft[-1]! + 1;
+      multCardsLeft[i] = multCardsLeft[i]! + 1;
     }
     //For the good mult cards, we add a lot of cards from x1.1-1.5, then have only one x2
-    cardCeiling = 6;
-    for (double i = 1.1; i <= 1.5; i += .1) {
-      for (double j = 1; j <= cardCeiling - (i - 1) * 10; j++) {
+    cardCeiling = 7;
+    for (double i = 1.05; i <= 1.3; i += .05) {
+      //For each mult card, the number of those cards depend on the formula
+      // max(1,cardCeiling - (i - 1)*2 * 10) . For example, for X1.1,
+      //we have max(1,cardCeiling (1.1-1)i* 1- ) . If cardCeiling = 8, this is
+      //max(1,8 - (.1) * 2 * 10) = 6
+
+      //Hence when the loop increaments by .1, the number of cards for that value decrements by 2 cards
+      for (double j = 1; j <= max(1, cardCeiling - (i - 1) * 2 * 10); j++) {
         deckList.add(MultCard(value: i));
-        cardsLeft[-1] = cardsLeft[-1]! + 1;
+        multCardsLeft[i] = multCardsLeft[i]! + 1;
       }
     }
     //Add the single x2 card
@@ -107,8 +192,10 @@ class CardDeck extends PositionComponent with TapCallbacks {
     // cardsLeft[-1] = cardsLeft[-1]! + 1;
   }
 
+  //The "positive effect for everyone" even action cards should be more plentiful
+  //the other
   void initEventActionCards() {
-    for (int i = 0; i < 3; i++) {
+    for (int i = 1; i <= 3; i++) {
       deckList.add(FreezeCard());
       deckList.add(FlipThreeCard());
       deckList.add(DoubleChanceCard());
@@ -122,7 +209,8 @@ class CardDeck extends PositionComponent with TapCallbacks {
       deckList.add(SunkProphet());
       deckList.add(ChoiceDraw());
       deckList.add(ReverseTurn());
-      cardsLeft[-1] = cardsLeft[-1]! + 13;
+
+      //Add to
     }
   }
 
@@ -131,13 +219,23 @@ class CardDeck extends PositionComponent with TapCallbacks {
     int numDiscardedCards = discardPile.length;
     for (int i = 0; i < numDiscardedCards; i++) {
       Card discardedCard = discardPile.removeLast();
+      deckList.add(discardedCard);
       if (discardedCard is NumberCard) {
         double value = discardedCard.value;
+        numberCardsLeft[value.toInt()] = numberCardsLeft[value.toInt()]! + 1;
+      } else if (discardedCard is PlusCard) {
         deckList.add(discardedCard);
-        cardsLeft[value.toInt()] = cardsLeft[value.toInt()]! + 1;
+        double value = discardedCard.value;
+        plusMinusCardsLeft[value.toInt()] =
+            plusMinusCardsLeft[value.toInt()]! + 1;
+      } else if (discardedCard is MinusCard) {
+        double value = discardedCard.value;
+        plusMinusCardsLeft[-1 * value.toInt()] =
+            plusMinusCardsLeft[-1 * value.toInt()]! + 1;
       } else {
-        deckList.add(discardedCard);
-        cardsLeft[-1] = cardsLeft[-1]! + 1;
+        //Currently there is no data structure seeing the number of each event action cards]
+        //left in deck, but there was,
+        //make changes here
       }
     }
     //After all cards from discard pile is added, reshuffle deck
@@ -156,10 +254,17 @@ class CardDeck extends PositionComponent with TapCallbacks {
     if (newCard is NumberCard) {
       double value = newCard.value;
       //Decrement cardsLeft
-      cardsLeft[value.toInt()] = cardsLeft[value.toInt()]! - 1;
+      numberCardsLeft[value.toInt()] = numberCardsLeft[value.toInt()]! - 1;
+    } else if (newCard is PlusCard) {
+      double value = newCard.value;
+      plusMinusCardsLeft[value.toInt()] =
+          plusMinusCardsLeft[value.toInt()]! + 1;
+    } else if (newCard is MinusCard) {
+      double value = newCard.value;
+      plusMinusCardsLeft[-1 * value.toInt()] =
+          plusMinusCardsLeft[-1 * value.toInt()]! + 1;
     } else {
-      //Else it's a value or event action card, so decrement -1
-      cardsLeft[-1] = cardsLeft[-1]! - 1;
+      //Potential data structure for event action cards, multiplication cards
     }
   }
 
