7f5860b678dc01f9248cf1f1e0cadc4e1f585313
Sean-Lee6 <seanlee.beaverton.or@gmail.com>
2025-09-24 21:02:48 -0700

    Added polarizer event

---
+++ lib/components/cards/event_cards/polarizer_card.dart
@@ -0,0 +1,25 @@
+//Choice draw: User can choose to reverse the polarity for any of the value action cards in their hand
+import 'package:six_seven/components/cards/card.dart';
+
+class Polarizer extends EventActionCard {
+  Polarizer();
+
+  @override
+  double executeOnStay(double currentValue) {
+    print("This function does nothing");
+    return currentValue;
+  }
+
+  @override
+  void executeOnEvent() {
+    //To do: implement
+    return;
+  }
+
+  @override
+  void description() {
+    print(
+      "${cardType.label} User can choose to reverse the polarity for any of the value action cards in their hand.",
+    );
+  }
+}
