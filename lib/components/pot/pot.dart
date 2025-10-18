d2e75a33e3bf7873eeef36b4627a3429a52140e7
methumk <methum.cg@gmail.com>
2025-10-17 22:58:32 -0700

    Updated pot to be able to reset withotu waiting on animation

--- lib/components/pot/pot.dart
+++ lib/components/pot/pot.dart
@@ -6,20 +6,32 @@ import 'package:six_seven/pages/game/game_screen.dart';
 import 'package:six_seven/utils/event_completer.dart';
 
 class Pot extends PositionComponent with HasGameReference<GameScreen> {
+  late final double startScore;
+  late double totalScore;
   late final CountingNumberComponent _totalScore;
   EventCompleter? _completer;
 
-  Pot({double startScore = 0, required Vector2 position, required Vector2 size})
-    : super(position: position, anchor: Anchor.bottomCenter, size: size) {
+  Pot({
+    required this.startScore,
+    required Vector2 position,
+    required Vector2 size,
+  }) : super(position: position, anchor: Anchor.bottomCenter, size: size) {
     _totalScore = CountingNumberComponent(
       value: startScore,
       duration: 1.0,
       position: size / 2 + position,
     );
+    totalScore = startScore;
+  }
+
+  void reset() {
+    totalScore = startScore;
+    _totalScore.updateNoAnimation(startScore);
   }
 
   Future<void> addToPot(double newScore) async {
-    _totalScore.updateValue(_totalScore.currValue + newScore);
+    totalScore += newScore;
+    _totalScore.updateValue(totalScore);
 
     // Wait for score animation to finish
     await _totalScore.completer.wait();
