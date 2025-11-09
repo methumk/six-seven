272d9975f53ee62273d79ff6d7f31688ba892e2d
methumk <methum.cg@gmail.com>
2025-11-08 17:25:30 -0800

    Choice draw - hand event and regular events both get redrawn from card

--- lib/components/cards/event_cards/choice_draw.dart
+++ lib/components/cards/event_cards/choice_draw.dart
@@ -230,8 +230,8 @@ class ChoiceDraw extends EventActionCard {
     if (selected is NumberCard) {
       print('Selected card is number getting added to number hand $selected');
       currPlayer.nch.addCardtoHand(selected);
-    } else if (selected is HandEventActionCard || selected is ValueActionCard) {
-      print('Selected card is getting added to DCH hand $selected');
+    } else if (selected is ValueActionCard) {
+      print('Value action getting added to DCH hand $selected');
       currPlayer.dch.addCardtoHand(selected);
     } else {
       print("Selected card is event card $selected");
