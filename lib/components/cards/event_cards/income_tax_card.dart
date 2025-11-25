6350c88c8ea20300229067859c36380477c2b483
Sean-Lee6 <seanlee.beaverton.or@gmail.com>
2025-11-25 12:48:27 -0800

    Implemented tax rate calculations for income tax event

--- lib/components/cards/event_cards/income_tax_card.dart
+++ lib/components/cards/event_cards/income_tax_card.dart
@@ -2,9 +2,10 @@
 import 'dart:async';
 
 import 'package:six_seven/components/cards/card.dart';
+import 'package:six_seven/components/players/player.dart';
 import 'package:six_seven/data/enums/event_cards.dart';
 
-class IncomeTax extends EventActionCard {
+class IncomeTax extends HandEventActionCard {
   IncomeTax() {
     eventEnum = EventCardEnum.IncomeTax;
   }
@@ -25,9 +26,113 @@ class IncomeTax extends EventActionCard {
     );
   }
 
+  //Grants Income tax
   @override
   Future<void> executeOnEvent() async {
-    //To do: implement
+    if (!cardUser!.hasIncomeTax) {
+      cardUser?.grantIncomeTax();
+      affectedPlayer = cardUser;
+    } else {
+      //See if there are any readily available players.
+      List<Player> worstPlayers = game.gameManager.totalLeaderBoard.bottomN(
+        game.gameManager.totalPlayerCount,
+      );
+      //If there is an available player, let affectedPlayer be that player.
+      //For CPU players, this affectedPlayer will be their choice for who
+      //will get Income tax.
+      for (Player player in worstPlayers) {
+        if (!player.isDone && !player.hasIncomeTax) {
+          affectedPlayer = player;
+          break;
+        }
+      }
+      //If no one is available to have Income tax,
+      //that is, affected user is still null, return early
+      if (affectedPlayer == null) {
+        print("No remaining player can have the Income tax card.");
+        game.gameManager.deck.addToDiscard([this]);
+        finishEventCompleter();
+        return;
+      }
+      //Else, there is an available player to give Income tax to.
+      //We don't have to worry about CPU because they will automatically choose the worst
+      //player available, in terms of total score, to give the Income tax to
+      //All that is left is to check if they are not CPU
+      else if (!cardUser!.isCpu()) {
+        bool validChoice = false;
+        while (!validChoice) {
+          await choosePlayer();
+          //If chosen player is active and has Income tax, it is valid!
+          if (!affectedPlayer!.hasIncomeTax && !affectedPlayer!.isDone) {
+            validChoice = true;
+          } else {
+            print("Chosen player is not valid. Please try again");
+          }
+        }
+        // We got user input. Signal to choosePlayer event is now completed.
+        //Then proceed with action using that input (stealingFromPlayer set)
+        //after the if statements
+      }
+      //If user is CPU, they already have an affectplayer, don't need a case for it
+    }
+    //Give Income tax to affected player
+    affectedPlayer!.grantIncomeTax();
+    await affectedPlayer?.onHit(this);
+    finishEventCompleter();
+    return;
+  }
+
+  //Method for determining tax rate, and changing current and total based on it
+  Future<void> playerTaxRate(Player currentPlayer) async {
+    List<Player> playerRankings = game.gameManager.totalCurrentLeaderBoard.topN(
+      game.gameManager.totalPlayerCount,
+    );
+    late int ranking;
+    for (int i = 0; i < playerRankings.length; i++) {
+      if (playerRankings[i] == currentPlayer) {
+        ranking = i + 1;
+        break;
+      }
+    }
+    //Tax System: 1.If player is best player in total + current value,  they get
+    //taxed the most at 20%, so their total and current score decreases by 20%.
+    //2.If player is in the worst ranking (ranking == total players), they get a tax refund of 20%, meaning
+    //their total value increases by 20%.
+    //3. If player is at, or above, the middle rank of (1+ total players)/ 2, their tax is 10%.
+    //4. If player is not top ranking and is below average in rankings,
+    //they are taxed at 6.7%
+
+    //best player
+    double averageRank = (1 + playerRankings.length) / 2;
+    if (ranking == 1) {
+      print(
+        "Player is number 1 in total value from previous rounds! 20% tax to total and current value",
+      );
+      currentPlayer.taxMultiplier *= .8;
+    } //Bottom player
+    else if (ranking == playerRankings.length) {
+      print(
+        "Player is in last place! They earn a tax refund of 20% to total and current value",
+      );
+      currentPlayer.taxMultiplier *= 1.2;
+    } //At, or better than average. Notice the smaller the ranking value, the better the rank is
+    else if (ranking <= averageRank) {
+      print(
+        "Player is at, or better than the middle rank! Player is not a top player, so 10% to total and current value",
+      );
+      currentPlayer.taxMultiplier *= .9;
+    } //Worse than average
+    else if (ranking > averageRank) {
+      print(
+        "Player is performing less than the middle rank! 6.7% tax to total and current value",
+      );
+      currentPlayer.taxMultiplier *= 1 - .067;
+    } //The following shouldn't happen; it means that player does not satisfy any of the cases
+    else {
+      print(
+        "Your player's ranking didn't meet any of the criteria, which shouldn't be possible. Rank:${ranking}. Please debug",
+      );
+    }
     return;
   }
 
