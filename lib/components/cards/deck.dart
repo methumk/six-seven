3b95b3865289fb703e9f529c7e957353871a53ef
Sean-Lee6 <seanlee.beaverton.or@gmail.com>
2025-12-01 21:23:22 -0800

    Deck changed to just income only for taxes

--- lib/components/cards/deck.dart
+++ lib/components/cards/deck.dart
@@ -217,53 +217,53 @@ class CardDeck extends PositionComponent with TapCallbacks {
   }
 
   void initEventActionCards() {
-    for (int i = 1; i <= 10; i++) {
-      deckList.add(FreezeCard());
+    for (int i = 1; i <= 100; i++) {
+      // deckList.add(FreezeCard());
       // // deckList.add(FlipThreeCard());
-      deckList.add(DoubleChanceCard());
-      deckList.add(TopPeekCard());
-      deckList.add(ThiefCard());
+      // deckList.add(DoubleChanceCard());
+      // deckList.add(TopPeekCard());
+      // deckList.add(ThiefCard());
       // // deckList.add(CribberCard());
-      deckList.add(ForecasterCard());
-      // deckList.add(IncomeTax());
+      // deckList.add(ForecasterCard());
+      deckList.add(IncomeTax());
       // deckList.add(SalesTax());
-      deckList.add(LuckyDieCard());
-      deckList.add(SunkProphet());
-      deckList.add(ChoiceDraw());
+      // deckList.add(LuckyDieCard());
+      // deckList.add(SunkProphet());
+      // deckList.add(ChoiceDraw());
       // // deckList.add(ReverseTurnCard());
-      deckList.add(DiscarderCard());
-      deckList.add(RedeemerCard());
+      // deckList.add(DiscarderCard());
+      // deckList.add(RedeemerCard());
       //Add to eventCardsLeft
-      eventCardsLeft[EventCardEnum.Freeze] =
-          eventCardsLeft[EventCardEnum.Freeze]! + 1;
+      // eventCardsLeft[EventCardEnum.Freeze] =
+      //     eventCardsLeft[EventCardEnum.Freeze]! + 1;
       // // eventCardsLeft[EventCardEnum.FlipThree] =
       // //     eventCardsLeft[EventCardEnum.FlipThree]! + 1;
-      eventCardsLeft[EventCardEnum.DoubleChance] =
-          eventCardsLeft[EventCardEnum.DoubleChance]! + 1;
-      eventCardsLeft[EventCardEnum.Thief] =
-          eventCardsLeft[EventCardEnum.Thief]! + 1;
-      eventCardsLeft[EventCardEnum.TopPeek] =
-          eventCardsLeft[EventCardEnum.TopPeek]! + 1;
+      // eventCardsLeft[EventCardEnum.DoubleChance] =
+      //     eventCardsLeft[EventCardEnum.DoubleChance]! + 1;
+      // eventCardsLeft[EventCardEnum.Thief] =
+      //     eventCardsLeft[EventCardEnum.Thief]! + 1;
+      // eventCardsLeft[EventCardEnum.TopPeek] =
+      //     eventCardsLeft[EventCardEnum.TopPeek]! + 1;
       // // eventCardsLeft[EventCardEnum.Cribber] =
       // //     eventCardsLeft[EventCardEnum.Cribber]! + 1;
-      eventCardsLeft[EventCardEnum.Forecaster] =
-          eventCardsLeft[EventCardEnum.Forecaster]! + 1;
-      // eventCardsLeft[EventCardEnum.IncomeTax] =
-      //     eventCardsLeft[EventCardEnum.IncomeTax]! + 1;
+      // eventCardsLeft[EventCardEnum.Forecaster] =
+      //     eventCardsLeft[EventCardEnum.Forecaster]! + 1;
+      eventCardsLeft[EventCardEnum.IncomeTax] =
+          eventCardsLeft[EventCardEnum.IncomeTax]! + 1;
       // eventCardsLeft[EventCardEnum.SalesTax] =
       //     eventCardsLeft[EventCardEnum.SalesTax]! + 1;
-      eventCardsLeft[EventCardEnum.LuckyDie] =
-          eventCardsLeft[EventCardEnum.LuckyDie]! + 1;
-      eventCardsLeft[EventCardEnum.SunkProphet] =
-          eventCardsLeft[EventCardEnum.SunkProphet]! + 1;
-      eventCardsLeft[EventCardEnum.ChoiceDraw] =
-          eventCardsLeft[EventCardEnum.ChoiceDraw]! + 1;
+      // eventCardsLeft[EventCardEnum.LuckyDie] =
+      //     eventCardsLeft[EventCardEnum.LuckyDie]! + 1;
+      // eventCardsLeft[EventCardEnum.SunkProphet] =
+      //     eventCardsLeft[EventCardEnum.SunkProphet]! + 1;
+      // eventCardsLeft[EventCardEnum.ChoiceDraw] =
+      //     eventCardsLeft[EventCardEnum.ChoiceDraw]! + 1;
       // // eventCardsLeft[EventCardEnum.ReverseTurn] =
       // //     eventCardsLeft[EventCardEnum.ReverseTurn]! + 1;
-      eventCardsLeft[EventCardEnum.Discarder] =
-          eventCardsLeft[EventCardEnum.Discarder]! + 1;
-      eventCardsLeft[EventCardEnum.Redeemer] =
-          eventCardsLeft[EventCardEnum.Redeemer]! + 1;
+      // eventCardsLeft[EventCardEnum.Discarder] =
+      //     eventCardsLeft[EventCardEnum.Discarder]! + 1;
+      // eventCardsLeft[EventCardEnum.Redeemer] =
+      //     eventCardsLeft[EventCardEnum.Redeemer]! + 1;
     }
   }
 
