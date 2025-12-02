0adc03072f58f87e5a49103bffb914a04ac1c3de
Sean-Lee6 <seanlee.beaverton.or@gmail.com>
2025-12-01 21:25:21 -0800

    Name change

--- lib/components/cards/event_cards/redeemer_card.dart
+++ lib/components/cards/event_cards/redeemer_card.dart
@@ -35,7 +35,7 @@ class RedeemerCard extends HandEventActionCard {
       affectedPlayer = cardUser;
     } else {
       //See if there are any readily available players.
-      List<Player> worstPlayers = game.gameManager.endGameLeaderBoard.bottomN(
+      List<Player> worstPlayers = game.gameManager.totalLeaderBoard.bottomN(
         game.gameManager.totalPlayerCount,
       );
       //If there is an available player, let affectedPlayer be that player.
