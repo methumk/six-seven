8bd846d9ef992a9d2f5b2fd649727138d2dac34e
methumk <methum.cg@gmail.com>
2026-01-25 22:15:06 -0800

    added pot label, and updated counting numbers for pot label

--- lib/components/pot/pot.dart
+++ lib/components/pot/pot.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
-
 import 'package:flame/components.dart';
+import 'package:flutter/rendering.dart';
 import 'package:six_seven/components/pot/counting_number.dart';
 import 'package:six_seven/pages/game/game_screen.dart';
 import 'package:six_seven/utils/event_completer.dart';
@@ -9,6 +9,7 @@ class Pot extends PositionComponent with HasGameReference<GameScreen> {
   late final double startScore;
   late double totalScore;
   late final CountingNumberComponent _totalScore;
+  late final TextComponent _potLabel;
   EventCompleter? _completer;
 
   Pot({
@@ -16,13 +17,27 @@ class Pot extends PositionComponent with HasGameReference<GameScreen> {
     required Vector2 position,
     required Vector2 size,
   }) : super(position: position, anchor: Anchor.bottomCenter, size: size) {
+    totalScore = startScore;
+    priority = -1;
+
     _totalScore = CountingNumberComponent(
       value: startScore,
       duration: 1.0,
-      position: size / 2 + position,
+      position: Vector2(0, size.y * -3),
+      anchor: Anchor.center,
+      textRenderer: TextPaint(
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      ),
+      priority: priority,
     );
-    totalScore = startScore;
-    priority = -1;
+
+    _potLabel = TextComponent(
+      text: "Pot",
+      anchor: Anchor.center,
+      position: Vector2.all(0),
+      textRenderer: TextPaint(style: TextStyle(fontSize: 25)),
+      priority: priority,
+    );
   }
 
   void reset() {
@@ -43,5 +58,6 @@ class Pot extends PositionComponent with HasGameReference<GameScreen> {
   FutureOr<void> onLoad() async {
     super.onLoad();
     add(_totalScore);
+    add(_potLabel);
   }
 }
