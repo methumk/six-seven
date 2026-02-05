25492f5e33ccd59548f19dc968ac9ef61b25cda8
methumk <methum.cg@gmail.com>
2026-02-04 22:25:12 -0800

    Updated render border to not over utilize render function

--- lib/components/rounded_border_component.dart
+++ lib/components/rounded_border_component.dart
@@ -1,5 +1,4 @@
 import 'package:flame/components.dart';
-import 'package:flame_svg/svg.dart';
 import 'package:flame_svg/svg_component.dart';
 import 'package:flutter/material.dart';
 
@@ -7,8 +6,11 @@ class RoundedBorderComponent extends PositionComponent with HasVisibility {
   double borderWidth;
   Color borderColor;
   Color? fillColor;
-  double borderRadius;
   SvgComponent? fillImage;
+  double borderRadius;
+
+  final Paint _borderPaint = Paint();
+  final Paint _fillPaint = Paint();
 
   RoundedBorderComponent({
     Vector2? position,
@@ -18,29 +20,44 @@ class RoundedBorderComponent extends PositionComponent with HasVisibility {
     this.borderRadius = 12.0,
     this.fillColor,
   }) {
-    this.position = position ?? Vector2.all(0);
-    this.size = size ?? Vector2.all(0);
+    this.position = position ?? Vector2.zero();
+    this.size = size ?? Vector2.zero();
+
+    _updatePaints();
   }
 
-  void setBorderColor(Color color) => borderColor = color;
-  void setFillColor(Color? color) => fillColor = color;
-  void setBorderWidth(double width) => borderWidth = width;
-  void setBorderRadius(double radius) => borderRadius = radius;
-  Future<void> setFillImage(SvgComponent svgImage) async {
-    try {
-      fillImage?.removeFromParent(); // remove old one if exists
-      fillImage = svgImage;
-      svgImage
-        ..position = Vector2.zero()
-        ..size = size
-        ..anchor = Anchor.topLeft;
-      fillImage = svgImage;
-      add(fillImage!);
+  void _updatePaints() {
+    _borderPaint
+      ..color = borderColor
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = borderWidth;
 
-      fillColor = null;
-    } catch (err) {
-      print("Caught error on setFillImage $err");
+    if (fillColor != null) {
+      _fillPaint
+        ..color = fillColor!
+        ..style = PaintingStyle.fill;
     }
+  }
+
+  void setBorderColor(Color color) {
+    borderColor = color;
+    _updatePaints();
+  }
+
+  void setFillColor(Color? color) {
+    fillColor = color;
+    _updatePaints();
+  }
+
+  void setFillImage(SvgComponent image) {
+    fillImage?.removeFromParent();
+    fillImage =
+        image
+          ..position = Vector2.zero()
+          ..size = size
+          ..anchor = Anchor.topLeft;
+    fillColor = null;
+    add(fillImage!);
   }
 
   @override
@@ -50,24 +67,10 @@ class RoundedBorderComponent extends PositionComponent with HasVisibility {
     final rect = Rect.fromLTWH(0, 0, size.x, size.y);
     final rrect = RRect.fromRectAndRadius(rect, Radius.circular(borderRadius));
 
-    canvas.save();
-    canvas.clipRRect(rrect);
-
     if (fillColor != null) {
-      final fillPaint =
-          Paint()
-            ..color = fillColor!
-            ..style = PaintingStyle.fill;
-      canvas.drawRRect(rrect, fillPaint);
+      canvas.drawRRect(rrect, _fillPaint);
     }
 
-    canvas.restore();
-
-    final borderPaint =
-        Paint()
-          ..color = borderColor
-          ..style = PaintingStyle.stroke
-          ..strokeWidth = borderWidth;
-    canvas.drawRRect(rrect, borderPaint);
+    canvas.drawRRect(rrect, _borderPaint);
   }
 }
